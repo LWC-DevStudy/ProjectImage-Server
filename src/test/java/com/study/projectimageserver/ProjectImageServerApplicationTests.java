7a912198acb2ed6d2430449cb04226d2491d76fc
timchae <timchae2@gmail.com>
2021-08-07 14:47:12 +0900

    Create project2

---
+++ src/test/java/com/study/projectimageserver/ProjectImageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.projectimageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectImageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
